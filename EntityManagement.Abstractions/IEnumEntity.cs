e2d6166b16e94e508058f5c2a2a092381ee36d3f
Saji Weerasingham <saji.weerasingham@outlook.com>
2019-01-02 07:52:58 +1000

    added enum entity interface

---
+++ EntityManagement.Abstractions/IEnumEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EntityManagement.Abstractions
+{
+    /// <summary>
+    /// Enum Entity Interface
+    /// </summary>
+    /// <typeparam name="TKey">Enum key type</typeparam>
+    public interface IEnumEntity<TKey> : IEntity<TKey>
+        where TKey : struct, IConvertible, IComparable
+    {
+        /// <summary>
+        /// Gets the enum item's name
+        /// </summary>
+        string Name { get; }
+    }
+}
